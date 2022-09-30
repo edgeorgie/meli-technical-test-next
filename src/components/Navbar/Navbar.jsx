9a4dceccaa31367683d0f06eae56faf1583f85fb
Edwin Jorge <ed.jorge1122@gmail.com>
2022-09-30 17:23:11 -0500

    EDIT: navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from 'assets/images/Logo_ML@2x.png'
+import search_icon from 'assets/icons/search_icon.png'
+import styles from './Navbar.module.scss'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+		<nav className={styles['navbar-container']}>
+			<Link href="/">
+				<a>
+					<Image width={60} height={38} src={logo} alt="Logo de Mercado Libre, manos estrechadas" />
+				</a>
+			</Link>
+			<form className={styles['navbar-form']}>
+				<input className={styles['navbar-form__input']} type="text" placeholder="Nunca dejes de buscar" />
+				<button className={styles['navbar-form__button']} type="submit">
+					<Image width={20} height={20} src={search_icon}></Image>
+				</button>
+			</form>
+		</nav>
+	)
 }
 
 export default Navbar
