621b0a178183a5e0c08e4175bae1f143729af4e7
Edwin Jorge <ed.jorge1122@gmail.com>
2022-10-05 00:14:01 -0500

    ADD: search item service for input suggestion in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react'
+import { searchItem } from 'services/searchItem'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from 'assets/images/Logo_ML@2x.png'
@@ -6,6 +8,16 @@ import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
 import styles from './Navbar.module.scss'
 
 const Navbar = () => {
+	const [items, setItems] = useState([])
+	const searchRef = useRef()
+
+	const handleChange = async () => {
+		const query = searchRef.current.value
+		const res = await searchItem(query)
+
+		setItems(res)
+	}
+
   return (
 		<header>
 			<nav className={styles['navbar-container']}>
@@ -15,15 +27,42 @@ const Navbar = () => {
 					</a>
 				</Link>
 				<form className={styles['navbar-form']}>
-					<input className={styles['navbar-form__input']} type="text" placeholder="Nunca dejes de buscar" />
+					<input ref={searchRef} className={styles['navbar-form__input']} type="text" placeholder="Nunca dejes de buscar" onChange={handleChange} />
 					<button className={styles['navbar-form__button']} type="submit">
 						<Image width={20} height={20} src={search_icon}></Image>
 					</button>
 				</form>
 			</nav>
+			{Boolean(items.length) && (
+				<div className={styles['navbar-search-sugestions']}>
+					<ul>
+						{items.map((item) => {
+							return (
+								<li key={item.id}>
+									<Link href={`items/${item.id}`}>
+										<a>{item.title}</a>
+									</Link>
+								</li>
+							)
+						})}
+					</ul>
+				</div>
+			)}
 			<Breadcrumb />
 		</header>
 	)
+}
+
+export async function getServerSideProps(context) {
+	const { query } = context
+	const { q = '' } = query
+
+	const { results } = await searchItem({ query: q })
+
+	return {
+		query: q,
+		results,
+	}
 }
 
 export default Navbar
---
+++ src/services/searchItem.js
@@ -0,0 +1,7 @@
+const API_SEARCH_ENDPOINT = 'https://meli-technical-test-backend-production-78cf.up.railway.app/api/items?q=:'
+
+export const searchItem = async (query) => {
+  const res = await fetch(`${API_SEARCH_ENDPOINT}${query}`).then(res => res.json())
+
+  return res.items
+}
